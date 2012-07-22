412b4043fc600c5e14d8bcca8843e024ab128e1a
steffmeister <software@steffmeister.at>
2012-07-22 11:16:14 +0200

    * fix invalid config string (again)

--- tmdbphp.php
+++ tmdbphp.php
@@ -102,7 +102,7 @@ class TMDBv3{
 
 		//Get Configuration
 		$conf = $this->getConfig();
-		if (empty($conf)){echo "Imposible leer configuracion, verifique que la llave de la API sea valida";exit;}
+		if (empty($conf)){echo "Unable to read configuration, please verify your API key!";exit;}
 		//set Images URL contain in config
 		$this->setImageURL($conf);
     }
@@ -146,10 +146,6 @@ class TMDBv3{
 		$trailer = $this->movieInfo($idMovie,"trailers",false);
 		// $trailer =$trailer['posters'];
 		return $trailer;
-	}
-	public function movieDetail($idMovie)
-	{
-		return $this->movieInfo($idMovie,"",false);
 	}
 
 	/**
