da0dd4c38477902da1b5bc7a8f235f272d7ade52
steffmeister <software@steffmeister.at>
2012-07-22 16:54:13 +0200

    * new example file tmdbexample.php, this is the one I use for reference (also removed the old example.php)

---
+++ tmdbexample.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html><head><title>tmdb tests</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+<?php
+
+require('tmdbphp.php');
+
+$tmdb_instance = new TMDbv3('YOUR API KEY HERE', 'en');
+
+echo '<a href="?page=search">searchMovie()</a> - <a href="?page=single">movieInfo() / movieDetail()</a> - <a href="?page=genres">Genres</a> - <a href="?page=latest">latestMovie()</a> - <a href="?page=nowplaying">nowPlayingMovies()</a> - <a href="?page=popular">popularMovieList()</a> - <a href="?page=config">getConfig()</a> - <a href="?page=moviecast">movieCast()</a> - <a href="?page=movieposter">moviePoster()</a> - <a href="?page=movieimages">movieImages()</a> - <a href="?page=imagetest">image test</a>';
+
+echo '<pre>';
+
+if ($_GET['page'] == 'search') {
+	print_r($tmdb_instance->searchMovie('Back to the future'));
+} else if ($_GET['page'] == 'latest') {
+	print_r($tmdb_instance->latestMovie());
+} else if ($_GET['page'] == 'nowplaying') {
+	print_r($tmdb_instance->nowPlayingMovies(1));
+} else if ($_GET['page'] == 'popular') {
+	print_r($tmdb_instance->popularMovieList(1));
+} else if ($_GET['page'] == 'genres') {
+	echo 'not in v3 yet';
+} else if ($_GET['page'] == 'config') {
+	print_r($tmdb_instance->getConfig());
+} else if ($_GET['page'] == 'moviecast') {
+	print_r($tmdb_instance->movieCast('105'));
+} else if ($_GET['page'] == 'movieposter') {
+	print_r($tmdb_instance->moviePoster('105'));
+} else if ($_GET['page'] == 'movieimages') {
+	print_r($tmdb_instance->movieImages('105'));
+} else if ($_GET['page'] == 'imagetest') {
+	$thumb_url = $tmdb_instance->getImageURL('w92');
+	$posters = $tmdb_instance->moviePoster('105');
+	echo $thumb_url.$posters[0]['file_path']."\n";
+	$orig_url = $tmdb_instance->getImageURL();
+	echo $orig_url.$posters[0]['file_path'];
+} else {
+	$movie = $tmdb_instance->movieInfo('105');
+	print_r($movie);
+	echo '=================';
+	//echo getMidCover($movie[0]);
+}
+
+echo '</pre>';
+
+?>
+</body>
+</html>
